5ad7dbf5b85184960f434138d1c44f0f55e5f8ba
4rc3us <juandanielarce398@gmail.com>
2022-04-09 13:37:47 -0500

    Config with Bubbles theme, fzf and PSReadline

---
+++ user_profile.ps1
@@ -0,0 +1,34 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt bubbles
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadlineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+	Get-Command -Name $command -ErrorAction SilentlyContinue |
+		Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+
+
