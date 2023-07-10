4cfd195823274bc41a5b0ec97cfcddecf813f4c4
Juan Daniel Arce <jdarce91@misena.edu.co>
2023-07-09 23:53:57 -0500

    erd tree alias

--- user_profile.ps1
+++ user_profile.ps1
@@ -6,7 +6,7 @@
 Invoke-Expression (&starship init powershell)
 
 # Icons
-Import-Module -Name Terminal-Icons
+# Import-Module -Name Terminal-Icons
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
@@ -29,11 +29,12 @@ Set-Alias l ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+#Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias x 'C:\Windows\explorer.exe .'
 Set-Alias lg lazygit
 Set-Alias lv lvim
-Set-Alias python python3
+Set-Alias python3 python
+Set-Alias lvim "$env:USERPROFILE\.local\bin\lvim.ps1"
 
 # Utilities
 function which ($command) {
@@ -41,17 +42,12 @@ function which ($command) {
 	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function w () { Set-Location ~/workspace/web/ }
 function ws () { Set-Location ~/workspace/ }
 function vd () { python.exe 'C:\Users\Juan.arce\Miniconda3\Scripts\vd' }
-function appdata () { Set-Location $env:LOCALAPPDATA }
-
 function bgn () { Start-Process -NoNewWindow -RedirectStandardOutput "NUL" @args }
 function bg () { Start-Process -NoNewWindow @args }
 
-function killP () { C:\Windows\system32\wsl.exe --terminate podman-machine-default }
-function killU () { C:\Windows\system32\wsl.exe --terminate Ubuntu-20.04 }
-function syncd () { "$HOME\.emacs.d\bin\doom sync -e" }
+function killF () { C:\Windows\system32\wsl.exe --terminate fedoraremix }
 
 function purge {
 	param([string]$path = $pwd)
@@ -61,25 +57,21 @@ function purge {
 	$lineasUnicas | Out-File -FilePath $path
 }
 
+function erds () {
+	erd  -s name -C none -y flat | bat
+}
+
 function pingt () { ping 8.8.8.8 -t }
 
 function gffs () { git flow feature start @args }
 
 function gfff () { git flow feature finish @args }
 
-function ssh-passwd () {
+<# function ssh-passwd () {
 	$empty = "The agent has no identities."
 	$answer = ssh-add -l
 
 	if ($answer -eq $empty) { sh-add $HOME\.ssh\id_ed25519 }
-}
+} #>
 
-ssh-passwd
-
-# PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-	param($commandName, $wordToComplete, $cursorPosition)
-	dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-	}
-}
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
