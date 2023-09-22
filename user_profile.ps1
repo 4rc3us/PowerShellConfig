12460b14656c9cbd19be48ea44a37e0737850f04
Juan Daniel <juandanielarce398@gmail.com>
2023-09-22 07:33:25 -0500

    Vi mode Psreadline

--- user_profile.ps1
+++ user_profile.ps1
@@ -9,7 +9,7 @@ Invoke-Expression (&starship init powershell)
 # Import-Module -Name Terminal-Icons
 
 # PSReadLine
-Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -EditMode vi
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Remove-PSReadLineKeyHandler -Chord Alt+1
@@ -18,23 +18,46 @@ Remove-PSReadLineKeyHandler -Chord Alt+3
 Set-PSReadLineKeyHandler -Chord 'alt+l' -Function ForwardWord
 Set-PSReadLineKeyHandler -Chord 'alt+h' -Function BackwardWord
 Set-PSReadlineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadlineKeyHandler -Chord 'Ctrl+u' -Function DeleteLine
+Set-PSReadlineKeyHandler -Chord 'Ctrl+w' -Function DeleteWord
+Set-PSReadlineKeyHandler -Chord 'Ctrl+e' -Function EndOfLine
+Set-PSReadlineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine
 
 # Fzf
 Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Alias
-Set-Alias vim nvim
-Set-Alias l ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-#Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Set-Alias x 'C:\Windows\explorer.exe .'
+Set-Alias x 'C:\Windows\explorer.exe'
 Set-Alias lg lazygit
+Set-Alias ld lazydocker
 Set-Alias lv lvim
 Set-Alias python3 python
-Set-Alias lvim "$env:USERPROFILE\.local\bin\lvim.ps1"
+Set-Alias -Name lvim -Value "$env:USERPROFILE\.local\bin\lvim.ps1"
+Set-Alias -Name .. -Value "cd.."
+
+function v () { nvim @args }
+function bat () { bat --theme base16 @args }
+function pipx () { python3 -m pipx @args }
+function erdf () { erd -s name -I -y inverted --hidden --no-git @args }
+function l () { lsd -1 @args }
+function ll () { lsd -1 -a @args }
+
+function touch (
+	[Parameter(Mandatory = $false, Position = 0)]
+	[string]$path = $pwd,
+
+	[Parameter(Mandatory = $true, Position = 1)]
+	[string]$name,
+
+	[Parameter(Mandatory = $false, Position = 2)]
+	[string]$value
+
+) {
+	New-Item -Path $path -ItemType File -Name $name -Value $value
+}
 
 # Utilities
 function which ($command) {
@@ -58,7 +81,7 @@ function purge {
 }
 
 function erds () {
-	erd  -s name -C none -y flat | bat
+	erd  -s name -C none -y flat
 }
 
 function pingt () { ping 8.8.8.8 -t }
@@ -75,3 +98,27 @@ function gfff () { git flow feature finish @args }
 } #>
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+function kubectl () {
+	minikube kubectl -- $args
+}
+
+# function TEST() {
+# 	$data = @{
+# 		"key1" = "value1"
+# 		"key2" = "value2"
+# 	}
+# 	while ($true) {
+# 		$response = Invoke-WebRequest -Uri "http://localhost:7080/" -Method Post -Body $data
+# 		Write-Output $response.Content
+# 		Start-Sleep -Seconds 1
+# 	}
+# }
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+	dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
